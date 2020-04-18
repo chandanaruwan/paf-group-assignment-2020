09b312217f1f05361bf86deefd69530f1363ba16
chandanaruwan <cdiyunuge93@gmail.com>
2020-04-18 19:20:45 +0530

    Patient Registration

---
+++ PafGroupProject/src/com/PatientService.java
@@ -0,0 +1,102 @@
+package com;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import model.Patient;
+
+
+@Path("/Patient")
+public class PatientService {
+	
+	
+	
+    Patient cusObj = new Patient();
+    
+    @GET
+    @Path("/")
+    @Produces(MediaType.TEXT_HTML)
+
+    public String readItems()
+     {
+       	
+    	
+    	return cusObj.readItems(); 
+    	//return "Patient Registration" ;
+    	}
+
+
+     
+    
+    
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertPatient(
+	@FormParam("fname") String fname,
+	@FormParam("lname") String lname,
+	@FormParam("nic") String nic,
+	@FormParam("sex") String sex,
+	@FormParam("address") String address,
+	@FormParam("phone") String phone)
+	{
+	String output = cusObj.insertPatient(fname,lname,nic,sex,address,phone);
+	return output;
+	}
+	
+	
+	
+	
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updatePatient(String itemData)
+	{
+	
+	JsonObject itemObject = new JsonParser().parse(itemData).getAsJsonObject();
+	
+	String patientID = itemObject.get("patientID").getAsString();
+	String fname = itemObject.get("fname").getAsString();
+	String lname = itemObject.get("lname").getAsString();
+	String nic = itemObject.get("nic").getAsString();
+	String sex = itemObject.get("sex").getAsString();
+	String address = itemObject.get("address").getAsString();
+	String phone = itemObject.get("phone").getAsString();
+
+	String output = cusObj.updatePatient(patientID,fname,lname,nic,sex,address,phone);
+	return output;
+	}
+	
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deletePatient(String itemData)
+	{
+	//Convert the input string to an XML document
+	Document doc = Jsoup.parse(itemData, "", Parser.xmlParser());
+	//Read the value from the element <itemID>
+	String patient = doc.select("patientID").text();
+	String output =cusObj.deletePatient(patient);
+	return output;
+	}
+}
+
+
---
+++ PafGroupProject/src/model/Patient.java
@@ -0,0 +1,189 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Patient {
+	
+
+	
+	private Connection connect()
+	{
+	Connection con = null;
+	try
+	{
+	Class.forName("com.mysql.jdbc.Driver");
+
+	con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/hospitalmanagement", "root", "");
+	}
+	catch (Exception e)
+	{
+		e.printStackTrace();
+	}
+	return con;
+	}
+	
+	
+	public String readItems()
+	{
+	String output = "";
+	try
+	{
+	Connection con = connect();
+	if (con == null)
+	{return "Error while connecting to the database for reading."; }
+	
+	output = "<table border=\"1\"><tr><th>PatientID</th><th>FirstName</th><th>LastName</th><th>Nic</th><th>Sex</th><th>Address</th><th>Phone</th><th>Update</th><th>Remove</th></tr>";
+	String query = "select * from customer";
+	Statement stmt = con.createStatement();
+	ResultSet rs = stmt.executeQuery(query);
+	
+	while (rs.next())
+	{
+	String patientID = Integer.toString(rs.getInt("patientID"));
+	String fname = rs.getString("fname");
+	String lname = rs.getString("lname");
+	String nic = rs.getString("nic");
+	String sex = rs.getString("sex");
+	String address = rs.getString("address");
+	String phone = rs.getString("phone");
+	
+	
+	output += "<tr><td>" + patientID + "</td>";
+	output += "<td>" + fname + "</td>";
+	output += "<td>" + lname + "</td>";
+	output += "<td>" + nic + "</td>";
+	output += "<td>" + sex + "</td>";
+	output += "<td>" +address + "</td>";
+	output += "<td>" + phone+ "</td>";
+	
+	/*output += "<td><input name=\"btnUpdate\" type=\"button\"value=\"Update\" class=\"btn btn-secondary\"></td>"+ "<td><form method=\"post\" action=\"patient.jsp\">"+ "<input name=\"btnRemove\" type=\"submit\" value=\"Remove\"class=\"btn btn-danger\">"
+	+ "<input name=\"patientID\" type=\"hidden\" value=\"" + patientID+ "\">" + "</form></td></tr>";
+	}
+	*/
+	
+	output += "<td><input name=\"btnUpdate\" type=\"button\"value=\"Update\" class=\"btn btn-secondary\"></td>"+ "<td><form method=\"post\" action=\"items.jsp\">"+ "<input name=\"btnRemove\" type=\"submit\" value=\"Remove\"class=\"btn btn-danger\">"
+			 + "</form></td></tr>";
+			}
+	con.close();
+	
+	
+	output += "</table>";
+	}
+	catch (Exception e)
+	{
+	output = "Error Occured reading the Customer Details";
+	System.err.println(e.getMessage());
+	}
+	return output;
+	}
+	
+	
+	
+	
+	
+	public String insertPatient(String fname, String lname, String nic, String sex,String address,String phone)
+	{
+	String output = "";
+	try
+	{
+	Connection con = connect();
+	if (con == null)
+	{return "Error while connecting to the database for inserting."; 
+	}
+	
+	
+	String query = " insert into customer(`fname`,`lname`,`nic`,`sex`,`address`,`phone`)"+ "values(?,?,?,?,?,?)";
+	PreparedStatement preparedStmt = con.prepareStatement(query);
+	//preparedStmt.setInt(1,0);
+	preparedStmt.setString(1,fname);
+	preparedStmt.setString(2,lname);
+	preparedStmt.setString(3,nic);
+	preparedStmt.setString(4,sex);
+	preparedStmt.setString(5,address);
+	preparedStmt.setString(6,phone);
+	
+	preparedStmt.execute();
+	con.close();
+	output = "Patient Details Inserted successfully";
+	}
+	catch (Exception e)
+	{
+	output = "Error while inserting the Patient Details.Check Again.";
+	System.err.println(e.getMessage());
+	}
+	return output;
+	
+	}
+	 
+	
+	public String updatePatient(String patientID, String fname, String lname, String nic, String sex,String address, String phone)
+	{
+	String output = "";
+	try
+	{
+	Connection con = connect();
+	if (con == null)
+	{
+		return "Error while connecting to the database for updating."; 
+	}
+	// create a prepared statement
+	
+	String query = "UPDATE customer SET patientID=?,fname=?,lname=?,nic=?,sex=?,address=?,phone=? WHERE patientID=?";
+	PreparedStatement preparedStmt = con.prepareStatement(query);
+	// binding values
+	preparedStmt.setString(1, fname);
+	preparedStmt.setString(2, lname);
+	preparedStmt.setString(3, nic);
+	preparedStmt.setString(4, sex);
+	preparedStmt.setString(5, address);
+	preparedStmt.setString(6, phone);
+	preparedStmt.setInt(7, Integer.parseInt(patientID));
+	// execute the statement
+	preparedStmt.execute();
+	con.close();
+	output = "Updated successfully";
+	}
+	catch (Exception e)
+	{
+	output = "Error Occured updating the Customer Details.";
+	System.err.println(e.getMessage());
+	}
+	return output;
+	}
+	
+	
+	
+	public String deletePatient(String patientID)
+	{
+	String output = "";
+	try
+	{
+	Connection con = connect();
+	if (con == null)
+	{return "Error while connecting to the database for deleting."; }
+	// create a prepared statement
+	String query = "delete from customer where patientID=?";
+	PreparedStatement preparedStmt = con.prepareStatement(query);
+	// binding values
+	preparedStmt.setInt(1, Integer.parseInt(patientID));
+	// execute the statement
+	preparedStmt.execute();
+	con.close();
+	output = "Deleted successfully";
+	}
+	catch (Exception e)
+	{
+	output = "Error while removing the Patient OF our Database. Check Again.";
+	System.err.println(e.getMessage());
+	}
+	return output;
+	}
+	
+	
+}
+
+
